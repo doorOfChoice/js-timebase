88550ededc7cf3820d13f27b0a93ab11b8ab5366
Wang Rui <kevinkiller766@gmail.com>
2016-11-24 20:35:21 +0800

    fixed the opacity animation problem

--- js-timebase.js
+++ js-timebase.js
@@ -3,7 +3,6 @@
 目前只能实现垂直方向的时间轴.
 
 @instructions
-  请在name_timeDateClick的外层再加一层块级元素
   之前一定要初始化      $().timebase(options)
 
 */
@@ -19,7 +18,7 @@ $.fn.timebase = function(options){
     name_timeSelected          : 'on',                      //value: 点击时间轴后属性;    elements : any name
     speed_date                 : 500,                       //value: 时间面板移动速度     elements: int
     speed_content              : 500,                       //value: 内容包含面板速度     elements: int
-    speed_opacity              : 200,                       //value: 透明度变化速度       elements: int
+    speed_opacity              : 500,                       //value: 透明度变化速度       elements: int
     attr_opacity               : '0.2',                     //value: 默认透明度          elements: string
     boolean_dateMove           : false,                     //value: 时间面板是否移动     elements: bool
     boolean_contentMove        : false,                     //value: 内容包含面板是否移动 elements: bool
@@ -31,7 +30,7 @@ $.fn.timebase = function(options){
 
   //判断DOM数据是否合法
   if($(setting.name_timeDates).length > 0 && $(setting.name_timeContents).length > 0){
-    var prevPosition  = 0;
+    var prevPosition    = 0;
     var selected        = false;                         //防止多次被点击
     var timeClickName   = setting.name_timeDates + ' ' + setting.name_timeDateClick;  //触发时间事件的属性
     var contentElements = $(setting.name_timeContents);  //内容面板的个数
@@ -54,12 +53,14 @@ $.fn.timebase = function(options){
           var sumDateLength = 0;
           //统计各自应该移动的高度
           for(var index = 0 ; index < currentIndex ; ++index){
-            sumDateLength += $(beforeDateElements[index]).height();
+            sumDateLength += $(beforeDateElements[index]).height() +
+                             parseInt($(beforeDateElements[index]).css('margin-bottom')) +
+                             parseInt($(beforeDateElements[index]).css('margin-top'));
           }
           //设置时间条移动动画
           $(setting.name_timeDates).animate({
             'marginTop' : -sumDateLength
-          }, setting.sleep_date);
+          }, setting.sleep_date, function(){selected = false;});
       }
     }
 
@@ -69,12 +70,14 @@ $.fn.timebase = function(options){
         var sumContentsLength = 0;
 
         for(var index = 0 ; index < currentIndex ; ++index){
-          sumContentsLength += $(contentElements[index]).height();
+          sumContentsLength += $(contentElements[index]).height() +
+                               parseInt($(contentElements[index]).css('margin-bottom')) +
+                               parseInt($(contentElements[index]).css('margin-top'));
         }
 
         $(setting.name_timeContentsContainer).animate({
           'marginTop' : -sumContentsLength
-        }, setting.sleep_content);
+        }, setting.sleep_content, function(){selected = false;});
       }
     }
 
@@ -99,9 +102,9 @@ $.fn.timebase = function(options){
           //非当前面板透明度设置为默认
           for(var index = 0 ; index < contentElements.length ; ++index){
             if(index != currentIndex){
-              $(contentElements[index]).animate({
-                'opacity' : setting.attr_opacity
-              }, setting.speed_opacity);
+              $(contentElements[index]).animate({'opacity' : setting.attr_opacity},
+              setting.speed_opacity,
+              function(){if(index == contentElements.length - 1)selected = false;});
             }
           }
        }
@@ -110,19 +113,20 @@ $.fn.timebase = function(options){
     /** 切换到上一个元素 **/
     function prevClick(){
       var currentDateElement = $(timeClickName + "." + 'on'),
-          prevDateElement    = currentDateElement.parent().prevAll().find(setting.name_timeDateClick);
+          prevDateElement    = currentDateElement.prevAll();
 
       if(prevDateElement.length != 0){
-        $($(prevDateElement.get(prevDateElement.length - 1))).click();
-      }     
+        $($(prevDateElement.get(0))).click();
+      }
     }
 
     /** 切换到下一个元素**/
     function nextClick(){
       var currentDateElement = $(timeClickName + "." + 'on'),
-          nextDateElement    = currentDateElement.parent().nextAll().find(setting.name_timeDateClick);
+          nextDateElement    = currentDateElement.nextAll();
+
       if(nextDateElement.length != 0){
-        $($(nextDateElement.get(nextDateElement.length - 1))).click();
+        $($(nextDateElement.get(0))).click();
       }
     }
 
@@ -140,10 +144,13 @@ $.fn.timebase = function(options){
     /** 绑定鼠标触发事件**/
     if(setting.boolean_scroll){
       $(document).scroll(function(event){
-        var currentPosition = document.body.scrollTop;
+        event.preventDefault();
+
+        var currentPosition = $(document).scrollTop();
         var delta  = currentPosition - prevPosition;
-        delta > 0 ? nextClick() : prevClick();
+        delta >= 0 ? nextClick() : prevClick();
         prevPosition = currentPosition;
+
       });
     }
 
@@ -151,10 +158,10 @@ $.fn.timebase = function(options){
     $(timeClickName).click(function(event){
         event.preventDefault();
         if(!selected){
-          var beforeDateElements = $(this).parent().prevAll();    //时间条当前元素前的所有元素
-          var currentIndex = beforeDateElements.length;           //当前name_timeDateClick位置
-          var currentDateElement = $(this);                       //当前name_timeDateClick元素
-          
+          var beforeDateElements = $(this).prevAll();    //时间条当前元素前的所有元素
+          var currentIndex = beforeDateElements.length;  //当前name_timeDateClick位置
+          var currentDateElement = $(this);              //当前name_timeDateClick元素
+
           selected = true;
 
           contentsOpacity(currentIndex);
@@ -162,7 +169,6 @@ $.fn.timebase = function(options){
           contentsTransition(currentIndex);
           dateMove(currentIndex, beforeDateElements);
 
-          selected = false;
 
           //标记当前元素被选择
           $(timeClickName).removeClass('on');
